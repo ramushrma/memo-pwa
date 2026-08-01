0ee47c8f816b1e22dcfeed4949ee04e4f44ce984
ramushrma <ramusharmagr@gmail.com>
2026-08-01 12:57:47 +0530

    Fix PWA paths for Vercel deployment

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,51 @@
-self.addEventListener('install',e=>self.skipWaiting());
+const CACHE_NAME = "memo-pwa-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./notes.html",
+  "./tasks.html",
+  "./about.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.svg",
+  "./icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return networkResponse;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
